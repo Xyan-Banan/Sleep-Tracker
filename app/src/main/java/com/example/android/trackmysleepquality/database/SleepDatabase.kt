2f9b7409750b45979f9b59ba7c0b6bef0288ec92
Mike <20vampir11@gmail.com>
2021-05-04 15:54:45 +0300

    Sleep database abstract class

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,48 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    /**
+     * Connects the database to the DAO.
+     */
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    /**
+     * Define a companion object, this allows us to add functions on the SleepDatabase class.
+     *
+     * For example, clients can call `SleepDatabase.getInstance(context)` to instantiate
+     * a new SleepDatabase.
+     */
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
